eea436508ad31445db7286639028cb2491f3bcea
StefanDerian <Stefan_sippo@yahoo.com>
2017-12-08 12:45:58 +1100

    the point test finished

--- PointTest-backend.php
+++ PointTest-backend.php
@@ -1,36 +1,239 @@
 <?php
 include('dbConnection.php');
 
-$Cage = $_POST["Cage"];
-$Gage = $_POST["Gage"];
-$Cenglish = $_POST["Cenglish"];
-$Genglish = $_POST["Genglish"];
-$Cwork = $_POST["Cwork"];
-$Gwork = $_POST["Gwork"];
-$Cqua = $_POST["Cqua"];
-$Gqua = $_POST["Gqua"];
-$Cstudy = $_POST["Cstudy"];
-$Gstudy = $_POST["Gstudy"];
-$Cnaati = $_POST["Cnaati"];
-$Gnaati = $_POST["Gnaati"];
-$Cpartner = $_POST["Cpartner"];
-$Gpartner = $_POST["Gpartner"];
-$Cpy = $_POST["Cpy"];
-$Gpy = $_POST["Gpy"];
-$Cstate = $_POST["Cstate"];
-$Gstate = $_POST["Gstate"];
-$Cfamily = $_POST["Cfamily"];
-$Gfamily = $_POST["Gfamily"];
-$Carea = $_POST["Carea"];
-$Garea = $_POST["Garea"];
-$Csum = $_POST["Csum"];
-$Gsum = $_POST["Gsum"];
+
 
 $id = isset($_GET['user'])?$_GET['user']:"";
 
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $query = "INSERT INTO point(Cage, Gage, Cenglish, Genglish, Cwork, Gwork, Cqua, Gqua, Cstudy, Gstudy, Cnaati, Gnaati, Cpartner, Gpartner, Cpy, Gpy, Cstate, Gstate, Cfamily, Gfamily, Carea, Garea, Csum, Gsum) VALUES ('$Carea', '$Gage', '$Cenglish', '$Genglish', '$Cwork', '$Gwork', '$Cqua', '$Gqua', '$Cstudy', '$Gstudy', '$Cnaati', '$Gnaati', '$Cpartner', '$Gpartner', ''$Cfamily', '$Gfamily', '$Carea', '$Garea')";
-    echo $query;
-    $mysqli->query($query);
+
+$result = $mysqli->query("SELECT * FROM pointtype JOIN clientpoint cp on cp.pointid = pointtype.id WHERE clientid = $id");
+
+$number = $result->num_rows;
+
+
+$formValue = array();
+
+$forms = array();
+
+$msg = "";
+
+
+function updateValue ($mysqli){
+	$id = $GLOBALS['id'];
+	$result = $mysqli->query("SELECT * FROM pointtype JOIN clientpoint cp on cp.pointid = pointtype.id WHERE clientid = $id");
+	
+	while ($row = mysqli_fetch_assoc($result)) {
+		# code...
+		
+		// $current = $row['formname']."['current']";
+		// //echo $current;
+
+		// $goal = $row['formname']."['goal']";
+		// //echo $current;
+
+		// $value = array(
+
+		// 	$row['formname'] => array(
+		// 		'current' => $row['current'],
+		// 		'goal' => $row ['goal']
+		// 	)
+		// );
+
+		// array_push($GLOBALS['formValue'],$value);
+		$GLOBALS['formValue'][$row['formname']] = array(
+			'current' => $row['current'],
+			'goal' => $row ['goal']
+		);
+
+	}
+	
 }
+
+
+
+
+$formList = $mysqli->query("SELECT * FROM pointtype");
+
+
+while ($row = mysqli_fetch_assoc($result)) {
+		# code...
+
+		// $current = $row['formname']."['current']";
+		// //echo $current;
+
+		// $goal = $row['formname']."['goal']";
+		// //echo $current;
+
+		// $value = array(
+
+		// 	$row['formname'] => array(
+		// 		'current' => $row['current'],
+		// 		'goal' => $row ['goal']
+		// 	)
+		// );
+
+		// array_push($GLOBALS['formValue'],$value);
+
+	$GLOBALS['forms'][$row['formname']] = array(
+		'id' => $row['id'],
+		'name' => $row ['name'],
+		'note' => $row ['note'],
+		'current' => $row ['current'],
+		'goal' => $row ['goal'],
+
+	);
+}
+
+
+function calculateTotals($id){
+
+
+	$sums = $GLOBALS['mysqli']->query("SELECT SUM(goal) as goal ,SUM(current) as current FROM clientpoint WHERE clientid = $id");
+
+	$result = mysqli_fetch_assoc($sums);
+
+	return $result;
+
+}
+
+// print_r($forms);
+
+if($number > 0){
+	$totals = calculateTotals($id);
+	updateValue($mysqli);
+	//print_r($formValue);
+}
+
+
+
+
+// if the clients has been assessed
+
+
+
+		# code...
+
+		// $current = $row['formname']."['current']";
+		// //echo $current;
+
+		// $goal = $row['formname']."['goal']";
+		// //echo $current;
+
+		// $value = array(
+
+		// 	$row['formname'] => array(
+		// 		'current' => $row['current'],
+		// 		'goal' => $row ['goal']
+		// 	)
+		// );
+
+		// array_push($GLOBALS['formValue'],$value);
+
+
+	//print_r($formValue);
+
+
+
+
+
+
+
+
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+	// $Cage = $_POST["Cage"];
+	// $Gage = $_POST["Gage"];
+	// $Cenglish = $_POST["Cenglish"];
+	// $Genglish = $_POST["Genglish"];
+	// $Cwork = $_POST["Cwork"];
+	// $Gwork = $_POST["Gwork"];
+	// $Cqua = $_POST["Cqua"];
+	// $Gqua = $_POST["Gqua"];
+	// $Cstudy = $_POST["Cstudy"];
+	// $Gstudy = $_POST["Gstudy"];
+	// $Cnaati = $_POST["Cnaati"];
+	// $Gnaati = $_POST["Gnaati"];
+	// $Cpartner = $_POST["Cpartner"];
+	// $Gpartner = $_POST["Gpartner"];
+	// $Cpy = $_POST["Cpy"];
+	// $Gpy = $_POST["Gpy"];
+	// $Cstate = $_POST["Cstate"];
+	// $Gstate = $_POST["Gstate"];
+	// $Cfamily = $_POST["Cfamily"];
+	// $Gfamily = $_POST["Gfamily"];
+	// $Carea = $_POST["Carea"];
+	// $Garea = $_POST["Garea"];
+	// $Csum = $_POST["Csum"];
+	// $Gsum = $_POST["Gsum"];
+
+	$query = "";
+
+
+
+
+	if($number > 0){
+		
+
+		//print_r($_POST);
+
+		foreach ($_POST as $key => $value) {
+		# code...
+			//if the client has been assessed, only update required 
+			$query = "UPDATE clientpoint SET ";
+			$pointid = $value['id'];
+			$goal = $value['goal'];
+			$current = $value['current'];
+			$query .= "current = $current, goal = $goal WHERE clientid = $id AND pointid = $pointid ";
+
+			$mysqli->query($query);
+		}
+		updateValue($mysqli);
+		$totals = calculateTotals($id);
+		updateValue($mysqli);
+		$msg = "The Point is successfully Updated";
+
+
+	}else{
+		//if the client has not been assessed
+		$query = "INSERT INTO clientpoint(pointid,current,goal,clientid) VALUES ";
+		foreach ($_POST as $key => $value) {
+		# code...
+			$pointid = $value['id'];
+			$goal = $value['goal'];
+			$current = $value['current'];
+			$query .= "('$pointid','$current','$goal','$id'),";
+			
+		}
+		$mysqli->query(substr($query,0, -1));
+		updateValue($mysqli);
+		$totals = calculateTotals($id);
+		updateValue($mysqli);
+		$msg = "The Point is successfully Inserted new record created";
+	}
+	
+
+	
+	
+	
+
+	// $query = "INSERT INTO point(Cage, Gage, Cenglish, Genglish, Cwork, Gwork, Cqua, Gqua, Cstudy, Gstudy, Cnaati, Gnaati, Cpartner, Gpartner, Cpy, Gpy, Cstate, Gstate, Cfamily, Gfamily, Carea, Garea, Csum, Gsum) VALUES ('$Carea', '$Gage', '$Cenglish', '$Genglish', '$Cwork', '$Gwork', '$Cqua', '$Gqua', '$Cstudy', '$Gstudy', '$Cnaati', '$Gnaati', '$Cpartner', '$Gpartner', ''$Cfamily', '$Gfamily', '$Carea', '$Garea')";
+	// echo $query;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- PointTest.php
+++ PointTest.php
@@ -5,122 +5,174 @@ include ('PointTest-backend.php');
 include ('header.php');
 ?>
 
+
+<?php if(!empty($msg)){ ?>
+<div class = "p-3 mb-2 bg-success text-white message">
+
+
+
+
+    <?php echo $msg; ?>
+
+    
+
+</div>
+<?php } ?>
+
+
 <?php if(isset($_SESSION['userID']) && isset($_GET['user'])){
     ?>
     <nav class="nav nav-tabs" id="myTab" role="tablist">
-        <a href=<?php echo "editAppt.php?user=$id"; ?> class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true" >Edit Data</a>
-        <a href=<?php echo "PointTest.php?user=$id"; ?> class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Point Test</a>
+        <a href=<?php echo "editAppt.php?user=$id"; ?> class="nav-item nav-link" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true" >Edit Data</a>
+        <a href=<?php echo "PointTest.php?user=$id"; ?> class="nav-item nav-link active" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Point Test</a>
     </nav>
     <?php } ?>
 
-<table width="100%" style="border-collapse:collapse" cellpadding="13">
-    <form method="post">
-        <tr>
-            <th></th>
-            <th>Skills</th>
-            <th>Current Points</th>
-            <th>Notes</th>
-            <th>Goal Points</th>
-        </tr>
-        <tr>
-            <td>1</td>
-            <td>Age</td>
-            <td><input id="Cage" name="Cage" type="number" value="<?php echo $Cage;?>" onChange="calculateTotal();" /></td>
-            <td>25-32 = 30 points;<br/>18-24 / 33-39 = 25 points;<br/>40-44 = 15 points</td>
-            <td><input name="Gage" type="number" value="<?php echo $Gage;?>" /></td>
-        </tr>
-        <tr>
-            <td>2</td>
-            <td>English language (IELTS / PTE / OET / TOBBLiBT / CAE)</td>
-            <td><input id="Cenglish" name="Cenglish" type="number" value="<?php echo $Cenglish;?>" onChange="calculateTotal();" /></td>
-            <td>IELTS 6 / PTE 50 = 0 points;<br/>IELTS 7 / PTE 65 = 10 points;<br/>IELTS 8 / PTE 65 = 20 points</td>
-            <td><input name="Genglish" type="number" value="<?php echo $Genglish;?>" /></td>
-        </tr>
-        <tr>
-            <td>3</td>
-            <td>Work experience</td>
-            <td><input name="Cwork" type="number" value="<?php echo $Cwork;?>" /></td>
-            <td>Australia: 1 year = 5 points;<br/>Overseas: 3 years = 5 points</td>
-            <td><input name="Gwork" type="number" value="<?php echo $Gwork;?>" /></td>
-        </tr>
-        <tr>
-            <td>4</td>
-            <td>Qualification</td>
-            <td><input name="Cqua" type="number" value="<?php echo $Cqua;?>" /></td>
-            <td>Trade / Diploma = 10 points;<br/>Bachelor / Master = 15 points;<br/>PHD = 20 points</td>
-            <td><input name="Gqua" type="number" value="<?php echo $Gqua;?>" /></td>
-        </tr>
-        <tr>
-            <td>5</td>
-            <td>2 Years full time study in Australia</td>
-            <td><input name="Cstudy" type="number" value="<?php echo $Cstudy;?>" /></td>
-            <td>5 points</td>
-            <td><input name="Gstudy" type="number" value="<?php echo $Gstudy;?>" /></td>
-        </tr>
-        <tr>
-            <td>6</td>
-            <td>NAATI</td>
-            <td><input name="Cnaati" type="number" value="<?php echo $Cnaati;?>" /></td>
-            <td>5 points</td>
-            <td><input name="gnaati" type="number" value="<?php echo $Gnaati;?>" /></td>
-        </tr>
-        <tr>
-            <td>7</td>
-            <td>Partner skills</td>
-            <td><input name="Cpartner" type="number" value="<?php echo $Cpartner;?>" /></td>
-            <td>5 points</td>
-            <td><input name="Gpartner" type="number" value="<?php echo $Gpartner;?>" /></td>
-        </tr>
-        <tr>
-            <td>8</td>
-            <td>Professional Year (Accounting / IT / Engineering)</td>
-            <td><input name="Cpy" type="number" value="<?php echo $Cpy;?>" /></td>
-            <td>5 points</td>
-            <td><input name="Gpy" type="number" value="<?php echo $Gpy;?>" /></td>
-        </tr>
-        <tr>
-            <td>9</td>
-            <td>State government sponsorship</td>
-            <td><input name="Cstate" type="number" value="<?php echo $Cstate;?>" /></td>
-            <td>5 points</td>
-            <td><input name="Gstate" type="number" value="<?php echo $Gstate;?>" /></td>
-        </tr>
-        <tr>
-            <td>10</td>
-            <td>Family sponsorship / regional Australia or Territory sponsorship</td>
-            <td><input name="Cfamily" type="number" value="<?php echo $Cfamily;?>" /></td>
-            <td>10 points</td>
-            <td><input name="Gfamily" type="number" value="<?php echo $Gfamily;?>" /></td>
-        </tr>
-        <tr>
-            <td>11</td>
-            <td>Study 2 years in Regional area</td>
-            <td><input name="Carea" type="number" value="<?php echo $Carea;?>" /></td>
-            <td>5 points</td>
-            <td><input name="Garea" type="number" value="<?php echo $Garea;?>" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td style="text-align:right">Total</td>
-            <td><input id="Csum" name="Csum" value="<?php $C=array('Cage', 'Cenglish', 'Cwork', 'Cqua', 'Cstudy', 'Cnaati', 'Cpartner', 'Cpy', 'Cstate', 'Cfamily', 'Carea'); echo array_sum($C);?>" /></td>
-            <td></td>
-            <td><input name="Gsum" value="<?php $G=array('Gage', 'Genglish', 'Gwork', 'Gqua', 'Gstudy', 'Gnaati', 'Gpartner', 'Gpy', 'Gstate', 'Gfamily', 'Garea'); echo array_sum($G);?>" />
-            </td>
-        </tr>
-    </form>
-</table>
+    <table width="100%" style="border-collapse:collapse" cellpadding="13">
+        <form method="post">
+            <tr>
+                <th></th>
+                <th>Skills</th>
+                <th>Current Points</th>
+                <th>Notes</th>
+                <th>Goal Points</th>
+            </tr>
 
-<script type="text/javascript">
-    
-    function calculateTotal() {
-        age = parseInt(document.getElementById("Cage").value);
-        english = parseInt(document.getElementById("Cenglish").value);
-        
-        total = document.getElementById("Csum");
-        
-        calculatedValue = (age + english);
-        total.value = calculatedValue;
-        
-    }
-</script>
+
+            <?php foreach ($forms as $key => $value) {?>
+            <tr>
+                <td><?php echo $value['id']; ?></td>
+                <td><?php echo $value['name']; ?></td>
+                <td><input id="Cage" name="<?php echo $key.'[current]';?>" type="number" value="<?php echo $formValue[$key]['current'];?>"/></td>
+                <td><?php echo $value['note']; ?></td>
+                <td><input name="<?php echo $key.'[goal]'; ?>" type="number" value="<?php echo $formValue[$key]['goal'];?>" /></td>
+                <td><input name="<?php echo $key.'[id]'; ?>" type="hidden" value=<?php echo $value['id']; ?> /></td>
+            </tr>
+
+
+
+            <?php } ?>
+
+           <!--  <tr>
+                <td>1</td>
+                <td>Age</td>
+                <td><input id="Cage" name="Age[current]" type="number" value="<?php echo $Age['current'];?>"/></td>
+                <td>25-32 = 30 points;<br/>18-24 / 33-39 = 25 points;<br/>40-44 = 15 points</td>
+                <td><input name="Age[goal]" type="number" value="<?php echo $formValue['Age']['goal'];?>" /></td>
+                <td><input name="Age[id]" type="hidden" value="1" /></td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>English language (IELTS / PTE / OET / TOBBLiBT / CAE)</td>
+                <td><input id="Cenglish" name="English[current]" type="number" value="<?php echo $English['current'];?>"/></td>
+                <td>IELTS 6 / PTE 50 = 0 points;<br/>IELTS 7 / PTE 65 = 10 points;<br/>IELTS 8 / PTE 65 = 20 points</td>
+                <td><input name="English[goal]" type="number" value="<?php echo $English['goal'];?>" /></td>
+                <td><input name="English[id]" type="hidden" value="2" /></td>
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Work experience</td>
+                <td><input name="Work[current]" type="number" value="<?php echo $Work['current'];?>" /></td>
+                <td>Australia: 1 year = 5 points;<br/>Overseas: 3 years = 5 points</td>
+                <td><input name="Work[goal]" type="number" value="<?php echo $Work['goal'];?>" /></td>
+                <td><input name="Work[id]" type="hidden" value="3" /></td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Qualification</td>
+                <td><input name="qua[current]" type="number" value="<?php echo $qua['current'];?>" /></td>
+                <td>Trade / Diploma = 10 points;<br/>Bachelor / Master = 15 points;<br/>PHD = 20 points</td>
+                <td><input name="qua[goal]" type="number" value="<?php echo $qua['goal'];?>" /></td>
+                <td><input name="qua[id]" type="hidden" value="4" /></td>
+            </tr>
+            <tr>
+                <td>5</td>
+                <td>2 Years full time study in Australia</td>
+                <td><input name="study[current]" type="number" value="<?php echo $study['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="study[goal]" type="number" value="<?php echo $study['goal'];?>" /></td>
+                <td><input name="study[id]" type="hidden" value="5" /></td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>NAATI</td>
+                <td><input name="naati[current]" type="number" value="<?php echo $naati['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="naati[goal]" type="number" value="<?php echo $naati['goal'];?>" /></td>
+                <td><input name="naati[id]" type="hidden" value="6" /></td>
+            </tr>
+            <tr>
+                <td>7</td>
+                <td>Partner skills</td>
+                <td><input name="partner[current]" type="number" value="<?php echo $partner['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="partner[goal]" type="number" value="<?php echo $partner['goal'];?>" /></td>
+                <td><input name="partner[id]" type="hidden" value="7" /></td>
+            </tr>
+            <tr>
+                <td>8</td>
+                <td>Professional Year (Accounting / IT / Engineering)</td>
+                <td><input name="py[current]" type="number" value="<?php echo $py['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="py[goal]" type="number" value="<?php echo $py['goal'];?>" /></td>
+                <td><input name="py[id]" type="hidden" value="8" /></td>
+            </tr>
+            <tr>
+                <td>9</td>
+                <td>State government sponsorship</td>
+                <td><input name="state[current]" type="number" value="<?php echo $state['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="state[goal]" type="number" value="<?php echo $state['goal'];?>" /></td>
+                <td><input name="state[id]" type="hidden" value="9" /></td>
+            </tr>
+            <tr>
+                <td>10</td>
+                <td>Family sponsorship / regional Australia or Territory sponsorship</td>
+                <td><input name="family[current]" type="number" value="<?php echo $family['current'];?>" /></td>
+                <td>10 points</td>
+                <td><input name="family[goal]" type="number" value="<?php echo $family['goal'];?>" /></td>
+                <td><input name="family[id]" type="hidden" value="10" /></td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>Study 2 years in Regional area</td>
+                <td><input name="area[current]" type="number" value="<?php echo $area['current'];?>" /></td>
+                <td>5 points</td>
+                <td><input name="area[goal]" type="number" value="<?php echo $area['goal'];?>" /></td>
+                <td><input name="area[id]" type="hidden" value="11" /></td>
+            </tr> -->
+            <tr>
+                <td></td>
+                <td style="text-align:right">Total</td>
+                <td>
+                    <?php echo isset($totals['current'])?$totals['current']:"0";?>
+                </td>
+                <td> 
+                    Total Goal
+                </td>
+                <td>
+                    <?php echo isset($totals['goal'])?$totals['goal']:"0";?> 
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input type = "submit" value = "submit" class = "btn btn-lg btn-primary pull-right">
+                </td>
+            </tr>
+        </form>
+    </table>
+
+    <!-- <script type="text/javascript">
+
+        function calculateTotal() {
+            age = parseInt(document.getElementById("Cage").value);
+            english = parseInt(document.getElementById("Cenglish").value);
+
+            total = document.getElementById("Csum");
+
+            calculatedValue = (age + english);
+            total.value = calculatedValue;
+
+        }
+    </script> -->
 </body>
