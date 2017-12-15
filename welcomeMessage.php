a8ba3c0036e338a8a18dd1610b32754991510ef2
StefanDerian <Stefan_sippo@yahoo.com>
2017-12-15 11:19:52 +1100

    welcome message

--- welcomeMessage.php
+++ welcomeMessage.php
@@ -2,5 +2,10 @@
 <?php
 include ('header.php');
 ?>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Thank You For Filling Up The Form</h1>
+		<p align="center"> Thank You for filing up the Form please wait until our staff take you to our professional consultant</p>
 
-<p> Thank You for filing up the Form please wait until our staff take you to our professional chouncellor</p>
+	</div>
+</div>
